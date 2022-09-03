a53446a4c76481f532998f865b9f6e0e0b55dc53
Seyoung Hong <maiorem00@gmail.com>
2022-09-03 20:57:07 +0900

    url query

--- web_start2/main.go
+++ web_start2/main.go
@@ -12,17 +12,23 @@ func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func barHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello bar!")
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "world"
+	}
+	fmt.Fprintf(w, "Hello %s!", name)
 }
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello World")
 	})
 
-	http.HandleFunc("/bar", barHandler)
+	mux.HandleFunc("/bar", barHandler)
 
-	http.Handle("/foo", &fooHandler{})
+	mux.Handle("/foo", &fooHandler{})
 
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", mux)
 }
