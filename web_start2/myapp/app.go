967291ed690261439090d9ac50d4ee90c137c7a4
Seyoung Hong <maiorem00@gmail.com>
2022-09-04 16:11:48 +0900

    test package and code

--- web_start2/myapp/app.go
+++ web_start2/myapp/app.go
@@ -14,6 +14,10 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
+}
+
 type fooHandler struct{}
 
 func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -44,9 +48,8 @@ func barHandler(w http.ResponseWriter, r *http.Request) {
 
 func NewHttpHandler() http.Handler {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello World")
-	})
+
+	mux.HandleFunc("/", indexHandler)
 
 	mux.HandleFunc("/bar", barHandler)
 
---
+++ web_start2/myapp/app_test.go
@@ -0,0 +1,22 @@
+package myapp
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIndexPathHandler(t *testing.T) {
+
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/", nil)
+
+	indexHandler(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+
+}
